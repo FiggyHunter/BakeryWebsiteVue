5b34cb3d997d9a12232a8a0cb5886df24369d06a
figgyhunter <leonardo.roi3@gmail.com>
2023-05-13 19:19:16 +0200

    implemented the news component

---
+++ components/news/NewsComponent.vue
@@ -0,0 +1,83 @@
+<template>
+  <article class="news">
+    <nuxt-img
+      loading="lazy"
+      src="https://mlinar.hr/images/news/mlinar_novosti_akcija_2023_05_02.jpg"
+      class="news__image"
+    ></nuxt-img>
+    <article class="news__content">
+      <h4 class="news__content__headline">{{ headline }}</h4>
+      <hr class="news__content__line" />
+      <p class="news__content__pgr">{{ content }}</p>
+    </article>
+  </article>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  headline: String,
+  content: String,
+});
+</script>
+
+<style lang="scss" scoped>
+@use '../../assets/styles/abstracts/variables' as *;
+
+.news {
+  font-family: $c-regular;
+  display: grid;
+  align-items: center;
+  margin-bottom: 3rem;
+  gap: 2rem;
+  img {
+    @media screen and (min-width: 700px) {
+      width: 100%;
+      place-self: center;
+    }
+    @media screen and (min-width: 900px) {
+      width: 100%;
+      place-self: center;
+      gap: 4rem;
+    }
+    @media screen and (min-width: 1200px) {
+      width: 80%;
+    }
+  }
+  @media screen and (min-width: 700px) {
+    margin-bottom: 5rem;
+  }
+
+  @media screen and (min-width: 1100px) {
+    margin-bottom: 8rem;
+  }
+
+  grid-template-columns: 1fr;
+  @media screen and (min-width: 1100px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    &__headline {
+      font-size: clamp(1rem, 0.1rem + 2.5vw, 1.75rem);
+      line-height: 1.75rem;
+      font-family: $c-bold;
+    }
+
+    &__pgr {
+      font-size: clamp(0.7rem, 0.4rem + 1vw, 1rem);
+    }
+
+    &__line {
+      width: 20%;
+      background-color: #f9b600;
+      height: 1px;
+      border: none;
+    }
+  }
+}
+</style>
+-
