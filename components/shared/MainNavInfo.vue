527d4b87780a0ca4b895c7525b64ae4b37cab72f
figgyhunter <leonardo.roi3@gmail.com>
2023-05-13 19:20:02 +0200

    users can now practically see how did they get to the page

---
+++ components/shared/MainNavInfo.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="nav-info">
+    <h3>{{ pageName }}</h3>
+    <div class="nav-info__extra">
+      <nuxt-link style="text-decoration: none" to="/">Home</nuxt-link>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-chevron-right"
+      >
+        <polyline points="9 18 15 12 9 6"></polyline>
+      </svg>
+      <nuxt-link>{{ pageName }}</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  pageName: { type: String, required: true },
+  pageRoute: { type: String, required: true },
+});
+</script>
+
+<style lang="scss" scoped>
+@use '../../assets/styles/abstracts/variables' as *;
+.nav-info {
+  margin: 15% 0 5% 0;
+  width: 100%;
+  text-align: center;
+  @media screen and (min-width: 900px) {
+    text-align: unset;
+  }
+  h3 {
+    font-family: $c-medium;
+  }
+  a {
+    color: #002559;
+  }
+  color: #002559;
+  font-family: $c-regular;
+  display: grid;
+  gap: 0.1rem;
+  grid-template-rows: 1fr 1fr;
+  &__extra {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    @media screen and (min-width: 900px) {
+      justify-content: unset;
+    }
+  }
+}
+</style>
