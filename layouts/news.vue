a3fdd7f7c342bb8dd7dea81dbae78ef0cd60749c
figgyhunter <leonardo.roi3@gmail.com>
2023-05-13 19:20:48 +0200

    added some placeholder text for the news component

---
+++ layouts/news.vue
@@ -0,0 +1,9 @@
+<template>
+  <SharedMainNavigation textcolor="black" />
+  <div class="div"><slot /></div>
+  <SharedMainFooter />
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
