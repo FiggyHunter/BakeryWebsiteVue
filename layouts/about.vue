988b25ac22e4eceebf0bc80cdc0aadb68fdfb40b
figgyhunter <leonardo.roi3@gmail.com>
2023-05-16 23:01:36 +0200

    created about route

---
+++ layouts/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <SharedMainNavigation textcolor="#002559" />
+  <slot />
+  <SharedMainFooter />
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
