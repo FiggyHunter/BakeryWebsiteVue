19fcb6008cb904b47304bc4ab34786589c35bd7b
figgyhunter <leonardo.roi3@gmail.com>
2023-05-17 23:35:47 +0200

    added text content by ChatGPT

--- pages/news.vue
+++ pages/news.vue
@@ -2,24 +2,34 @@
   <div class="text">
     <SharedMainNavInfo route-name="News" page-name="News" page-route="/news" />
     <NewsComponent
-      headline="Ne propustite novu dvotjednu akciju!"
-      content="Donosimo vam neodoljive proizvode po odličnim cijenama tijekom razdoblja od 1.5. do 14.5. Ne propustite ovu sjajnu priliku i posjetite nas kako biste se počastili sljedećim proizvodima: Kukuruzna kiflica 30g po cijeni od samo 0.20 eura - savršen mali snack za svaku prigodu. Extreme choco muffin i Red velvet muffin 110g po cijeni od samo 1.25 eura - savršeno za sve koji žele uživati u ukusnom desertu. Kruh s bučinim sjemenkama 500g po cijeni od 1.60 eura - ukusan i hranjiv izbor za sve koji traže zdraviju opciju kruha. Pizza calzone 220g + Coca Cola 0.5L po cijeni od samo 2 eura – dobitna kombinacija za brzinski ručak uz omiljeni napitak. Šunka sir košarica 105g po cijeni od samo 1 eura – savršen izbor za užinu."
-      imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Flag_of_Bosnia_and_Herzegovina.svg/1920px-Flag_of_Bosnia_and_Herzegovina.svg.png"
+      headline="We won the competition!"
+      content="Breaking news! Our bakery has just won first place in the annual citywide baking competition for our famous chocolate cake. Our head baker, Jane, spent months perfecting the recipe, and it has paid off. The judges raved about the cake's rich, moist texture and perfectly balanced chocolate flavor. We are thrilled to be recognized for our hard work and dedication to quality baking. To celebrate our victory, we're offering a special discount on our award-winning chocolate cake for a limited time. Come and taste the best chocolate cake in town!"
+      :imageUrl="`${base}/bakerscompetition.webp`"
     />
     <NewsComponent
-      headline="Ne propustite novu dvotjednu akciju!"
-      content="Donosimo vam neodoljive proizvode po odličnim cijenama tijekom razdoblja od 1.5. do 14.5. Ne propustite ovu sjajnu priliku i posjetite nas kako biste se počastili sljedećim proizvodima: Kukuruzna kiflica 30g po cijeni od samo 0.20 eura - savršen mali snack za svaku prigodu. Extreme choco muffin i Red velvet muffin 110g po cijeni od samo 1.25 eura - savršeno za sve koji žele uživati u ukusnom desertu. Kruh s bučinim sjemenkama 500g po cijeni od 1.60 eura - ukusan i hranjiv izbor za sve koji traže zdraviju opciju kruha. Pizza calzone 220g + Coca Cola 0.5L po cijeni od samo 2 eura – dobitna kombinacija za brzinski ručak uz omiljeni napitak. Šunka sir košarica 105g po cijeni od samo 1 eura – savršen izbor za užinu."
-      imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Flag_of_Bosnia_and_Herzegovina.svg/1920px-Flag_of_Bosnia_and_Herzegovina.svg.png"
+      headline="New Exclusive Coffee Drinks In Our Bakery!"
+      content="Exciting announcement! Our bakery has just partnered with a local coffee roaster to offer a new line of specialty coffee drinks. From lattes to cappuccinos, we're now using freshly roasted beans to create the perfect cup of coffee to pair with our baked goods. Our customers can now enjoy the aroma of freshly brewed coffee while indulging in our signature treats. We're committed to providing the best quality ingredients in all of our products, and this partnership is just one more step in that direction. Come and try our new coffee menu today!"
+      :imageUrl="`${base}/coffee.webp`"
     />
     <NewsComponent
-      headline="Ne propustite novu dvotjednu akciju!"
-      content="Donosimo vam neodoljive proizvode po odličnim cijenama tijekom razdoblja od 1.5. do 14.5. Ne propustite ovu sjajnu priliku i posjetite nas kako biste se počastili sljedećim proizvodima: Kukuruzna kiflica 30g po cijeni od samo 0.20 eura - savršen mali snack za svaku prigodu. Extreme choco muffin i Red velvet muffin 110g po cijeni od samo 1.25 eura - savršeno za sve koji žele uživati u ukusnom desertu. Kruh s bučinim sjemenkama 500g po cijeni od 1.60 eura - ukusan i hranjiv izbor za sve koji traže zdraviju opciju kruha. Pizza calzone 220g + Coca Cola 0.5L po cijeni od samo 2 eura – dobitna kombinacija za brzinski ručak uz omiljeni napitak. Šunka sir košarica 105g po cijeni od samo 1 eura – savršen izbor za užinu."
-      imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Flag_of_Bosnia_and_Herzegovina.svg/1920px-Flag_of_Bosnia_and_Herzegovina.svg.png"
+      headline="Bread like no other!"
+      content="Fresh from the oven! Our bakery is
+    excited to announce a new line of artisanal bread that uses locally sourced and organic
+    ingredients. Our bakers have been experimenting with different types of flours, grains, and
+    seeds to create unique and flavorful bread that's not available anywhere else. From whole wheat
+    to rye and sourdough, our bread is made with traditional techniques and baked in a stone oven
+    for that perfect crust and aroma. We believe that good bread should be accessible to everyone,
+    and that's why we're offering our new bread at affordable prices. Come and try our fresh bread
+    and taste the difference in every slice. You can also pre-order for pick up or delivery, so you
+    can enjoy fresh bread anytime, anywhere!"
+      :imageUrl="`${base}/bread.webp`"
     />
   </div>
 </template>
 
 <script setup lang="ts">
+const base = useRuntimeConfig().imagesBase;
+
 definePageMeta({
   layout: 'news',
 });
