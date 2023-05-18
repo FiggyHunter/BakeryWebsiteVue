c4f9b038ed252d0d3be850b3651814f381e9bc73
figgyhunter <leonardo.roi3@gmail.com>
2023-05-19 00:15:29 +0200

    implemented dynamic page route

---
+++ components/products/ProductsPage.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="limiter">
+    <SharedMainNavInfo class="info" page-name="Our Products" route-name="Products" />
+    <main class="products">
+      <aside class="products__filters"
+        ><button
+          :class="[`products__filters__button`, { bold: category === selectedCategory }]"
+          @click="updateCategory(category)"
+          v-for="category in categories"
+          :key="category"
+        >
+          {{ category }}</button
+        ></aside
+      >
+      <section class="products__displayed-products">
+        <ProductsCard
+          v-for="product in displayedProducts"
+          :key="product.id"
+          class="products__displayed-product"
+          :img="product.imageURL"
+          :name="product.productName"
+          :id="product.id"
+        />
+      </section>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useProductsStore } from '~/stores/products';
+
+const productsStore = useProductsStore();
+
+const categories = ref();
+const selectedCategory = ref('');
+const fetchedProducts = ref();
+
+const updateCategory = (category: string) => {
+  selectedCategory.value = category;
+};
+
+onBeforeMount(async () => {
+  await productsStore.FETCH_PRODUCTS();
+  fetchedProducts.value = productsStore.GET_ALL_PRODUCTS;
+  categories.value = productsStore.GET_PRODUCTS_CATEGORIES;
+  selectedCategory.value = 'breads';
+});
+
+const displayedProducts = computed(() => {
+  return selectedCategory.value === ''
+    ? fetchedProducts.value
+    : fetchedProducts.value[selectedCategory.value];
+});
+</script>
+
+<style lang="scss" scoped>
+@use '../../assets/styles/abstracts/variables' as *;
+
+.info {
+  margin-top: 30vw;
+  @media screen and (min-width: 700px) {
+    margin-top: 10vw;
+    margin-left: 6%;
+  }
+}
+.limiter {
+  width: 95%;
+  @media screen and (min-width: 900px) {
+    width: 85%;
+  }
+  margin: 0 auto;
+}
+
+.products {
+  font-family: $c-regular;
+  display: grid;
+  grid-template-columns: 2fr 5fr;
+  @media screen and (min-width: 700px) {
+    grid-template-columns: 1fr 5fr;
+  }
+  width: 100%;
+  margin: 2rem 0rem;
+  height: fit-content;
+  position: relative;
+  &__filters {
+    position: sticky;
+    top: clamp(80px, 100px + 4vw, 200px);
+    button {
+      position: sticky;
+      text-transform: uppercase;
+      font-family: $c-medium;
+      width: fit-content;
+      justify-self: center;
+      font-size: 1.3rem;
+    }
+    display: grid;
+    grid-template-columns: 1fr;
+    height: fit-content;
+    gap: 2rem;
+  }
+  &__displayed-products {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, 250px);
+    @media screen and (min-width: 1000px) {
+      grid-template-columns: repeat(auto-fit, 400px);
+    }
+    justify-content: center;
+    gap: 2rem;
+  }
+}
+
+.bold {
+  font-weight: 900;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="product-content">
+    <nuxt-img class="product-content__image" src="fdfdfd"></nuxt-img>
+    <section class="product-content__description">
+      <h1>{{ displayedProduct.productName }}</h1>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { copyFile } from 'fs';
+import { getProductById } from '../../api/getProductById';
+const fetchedProduct = ref();
+const displayedProduct = computed(() => {
+  return new Object(fetchedProduct.value);
+});
+const route = useRoute();
+onBeforeMount(async () => {
+  fetchedProduct.value = await getProductById(Number(route.params.id));
+});
+</script>
+
+<style lang="scss" scoped></style>
