f87831f39219ef5156e0c271585852d7d1582586
figgyhunter <leonardo.roi3@gmail.com>
2023-05-19 00:16:34 +0200

    updated config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
-const getBaseUrl = () => {
+const getImagesBaseUrl = () => {
   return process.env.ENV === 'DEV' ? 'http://localhost:3000/images' : 'https://www.leotg.com';
+};
+
+const getProductsBaseUrl = () => {
+  return process.env.ENV === 'DEV'
+    ? 'http://localhost:4000/api/products/'
+    : 'https://www.leotg.com';
+};
+
+const getProductsImagesBaseUrl = () => {
+  return process.env.ENV === 'DEV'
+    ? 'http://localhost:3000/images/products/'
+    : 'https://www.leotg.com';
 };
 
 export default {
@@ -10,11 +22,27 @@ export default {
     'vuetify/lib/styles/main.sass',
   ],
   build: {
-    transpile: ['vuetify'],
+    transpile: ['vuetify', 'VueNumber'],
+    compilerOptions: {
+      // List of custom element tag names
+      isCustomElement: (tag) => tag.startsWith('VueNumber'),
+    },
   },
   runtimeConfig: {
     public: {
-      imagesBase: getBaseUrl(),
+      imagesBase: getImagesBaseUrl(),
+      productsBase: getProductsBaseUrl(),
+      productsImagesBase: getProductsImagesBaseUrl(),
     },
+  },
+  modules: ['nuxt-quasar-ui', '@nuxt/image-edge', '@pinia/nuxt'],
+  plugins: [{ src: '@/plugins/VueNumber.js', mode: 'client' }],
+  quasar: {
+    extras: {
+      fontIcons: ['material-icons'],
+    },
+  },
+  server: {
+    host: '0.0.0.0',
   },
 };
