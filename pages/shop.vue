e175d5af41a0a7b12a1df48d797ca8085526d143
figgyhunter <leonardo.roi3@gmail.com>
2023-05-19 15:25:45 +0200

    added placeholder for shop

--- pages/shop.vue
+++ pages/shop.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>This is shop.</div>
+  <div class="placeholder">Work in progress...</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+definePageMeta({ layout: 'about' });
+</script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.placeholder {
+  height: 100vh;
+  display: grid;
+  place-content: center;
+  text-transform: uppercase;
+  font-size: clamp(1rem, 1rem + 3vw, 2rem);
+}
+</style>
