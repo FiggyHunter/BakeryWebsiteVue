8051dc623be1b48600cc4df089e5b7d40b85a6d5
figgyhunter <leonardo.roi3@gmail.com>
2023-05-19 15:25:36 +0200

    added placeholder for cart

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>This is the cart.</div>
+  <div class="placeholder">Work in progress...</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+definePageMeta({ layout: 'about' });
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.placeholder {
+  height: 100vh;
+  display: grid;
+  place-content: center;
+  text-transform: uppercase;
+  font-size: clamp(1rem, 1rem + 3vw, 2rem);
+}
+</style>
