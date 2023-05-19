b33c1d62faef1f3ee9b5d7f0c8f149d2c3330426
figgyhunter <leonardo.roi3@gmail.com>
2023-05-19 15:25:03 +0200

    finished products card

--- components/products/ProductsCard.vue
+++ components/products/ProductsCard.vue
@@ -6,8 +6,8 @@
         class="products__displayed-product__image"
         format="webp"
       ></nuxt-img>
-      <div class="products__displayed-product__icons"
-        >placeholder for iconsplaceholder for iconsplaceholder for icons</div
+      <div class="products__displayed-product__icons">
+        <div v-for="category in sub" :key="category" class="">{{ category }}</div></div
       >
       <h3 class="products__displayed-product__headline">{{ name }}</h3>
     </nuxt-link>
@@ -22,10 +22,12 @@ defineProps({
   test: { type: String },
   img: { type: String },
   name: { type: String },
+  sub: { type: String },
 });
 </script>
 
 <style lang="scss" scoped>
+@use '../../assets/styles/abstracts/variables' as *;
 article {
   border: 3px solid #002559;
   background-color: #002559;
@@ -35,20 +37,27 @@ article {
     text-decoration: none;
     color: unset;
     display: grid;
-    grid-template-rows: 250px 100px 50px;
+    grid-template-rows: min-content min-content auto;
+    gap: 0.5rem;
     grid-template-columns: 1fr;
     height: 400px;
   }
   .products__displayed-product__image {
     width: 500px;
-    height: 250px;
+    height: 260px;
     object-fit: cover;
   }
 
   .products__displayed-product__icons {
-    align-self: center;
+    align-self: start;
     text-align: center;
     color: white;
+    display: flex;
+    justify-content: center;
+    gap: 0.5rem;
+    text-transform: uppercase;
+    font-family: $c-medium;
+    flex-direction: column;
   }
   .products__displayed-product__headline {
     font-size: clamp(1rem, 1rem + 1vw, 1.75rem);
@@ -57,6 +66,7 @@ article {
     line-height: 1rem;
     align-self: end;
     margin-bottom: 1rem;
+    font-family: $c-bold;
   }
 }
 </style>
