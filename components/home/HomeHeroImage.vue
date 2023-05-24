53c7e62f0dc599297fb17f50fec3597c219beb1b
figgyhunter <leonardo.roi3@gmail.com>
2023-05-24 22:57:19 +0200

    transformed image on home into the component

---
+++ components/home/HomeHeroImage.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <img class="image" src="../../assets/images/bakery.avif" alt="" srcset="" />
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.image {
+  object-fit: cover;
+  width: 100vw;
+  height: 100vh;
+  filter: brightness(50%);
+}
+</style>
