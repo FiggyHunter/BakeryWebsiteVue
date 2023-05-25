e94ad23750299c5ba575c096dfee743088786bcd
figgyhunter <leonardo.roi3@gmail.com>
2023-05-25 19:43:13 +0200

    logic of notifying the user can now be applied to the entire app

--- stores/user.ts
+++ stores/user.ts
@@ -3,6 +3,7 @@ import { getProducts } from '../api/getProducts';
 import type { ProductAPIResponse } from '../api/types';
 
 export const useUserStore = defineStore('user', () => {
+  const $q = useQuasar();
   const selectedCategory = ref('');
 
   const ADD_SELECTED_CATEGORY = (category: string) => {
@@ -13,5 +14,21 @@ export const useUserStore = defineStore('user', () => {
     return selectedCategory.value;
   });
 
-  return { selectedCategory, ADD_SELECTED_CATEGORY, GET_SELECTED_CATEGORY };
+  const ADD_PRODUCT_IN_CART = function (product) {
+    $q.notify({
+      color: 'indigo-10',
+      textColor: 'yellow-7',
+      message: `You added ${product.productName} `,
+      position: 'bottom',
+      actions: [
+        {
+          label: 'Dismiss',
+          color: 'white',
+        },
+      ],
+      timeout: 4000,
+    });
+  };
+
+  return { selectedCategory, ADD_SELECTED_CATEGORY, GET_SELECTED_CATEGORY, ADD_PRODUCT_IN_CART };
 });
