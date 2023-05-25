2662db65fe23783be000603a1f94e81830be8cba
figgyhunter <leonardo.roi3@gmail.com>
2023-05-25 19:44:42 +0200

    added a new layout for [id]

---
+++ layouts/product-id.vue
@@ -0,0 +1,12 @@
+<template>
+  <SharedMainNavigation textcolor="white" />
+  <div><slot /></div>
+  <SharedMainFooter />
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+a {
+}
+</style>
