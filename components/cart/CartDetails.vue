3d28e1226ee493f87c1103d898cfa68576f1a38b
figgyhunter <leonardo.roi3@gmail.com>
2023-05-30 23:39:57 +0200

    started cart

---
+++ components/cart/CartDetails.vue
@@ -0,0 +1,17 @@
+<template>
+  <article class="cart-item"> {{ pName }}</article>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  pName: { type: String, required: true },
+  pPrice: { type: String, required: true },
+  pQuantity: { type: String, required: true },
+});
+</script>
+
+<style lang="scss" scoped>
+.cart-item {
+  background-color: blue;
+}
+</style>
