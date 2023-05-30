f5b0a47ec2d9bb7849d873bb0ead443f80f06d41
figgyhunter <leonardo.roi3@gmail.com>
2023-05-30 23:34:16 +0200

    started logic for the cart

--- stores/user.ts
+++ stores/user.ts
@@ -5,6 +5,7 @@ import type { ProductAPIResponse } from '../api/types';
 export const useUserStore = defineStore('user', () => {
   const $q = useQuasar();
   const selectedCategory = ref('');
+  const cart = ref([]);
 
   const ADD_SELECTED_CATEGORY = (category: string) => {
     selectedCategory.value = category;
@@ -14,11 +15,17 @@ export const useUserStore = defineStore('user', () => {
     return selectedCategory.value;
   });
 
+  const GET_CART_PRODUCTS = computed(() => {
+    return cart;
+  });
+
   const ADD_PRODUCT_IN_CART = function (product) {
+    cart.value.push(product);
+    console.log(cart.value);
     $q.notify({
       color: 'indigo-10',
       textColor: 'yellow-7',
-      message: `You added ${product.productName} `,
+      message: `You added ${product.name} `,
       position: 'bottom',
       actions: [
         {
@@ -30,5 +37,11 @@ export const useUserStore = defineStore('user', () => {
     });
   };
 
-  return { selectedCategory, ADD_SELECTED_CATEGORY, GET_SELECTED_CATEGORY, ADD_PRODUCT_IN_CART };
+  return {
+    selectedCategory,
+    ADD_SELECTED_CATEGORY,
+    GET_SELECTED_CATEGORY,
+    ADD_PRODUCT_IN_CART,
+    GET_CART_PRODUCTS,
+  };
 });
