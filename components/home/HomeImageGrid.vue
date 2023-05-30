142ccbc8150172720a556b319fce7979c0838252
figgyhunter <leonardo.roi3@gmail.com>
2023-05-30 23:39:07 +0200

    updated image grid to be a smaller width

--- components/home/HomeImageGrid.vue
+++ components/home/HomeImageGrid.vue
@@ -1,22 +1,22 @@
 <template>
   <section class="grid-images">
     <HomeImageComponent
-      card-title="News from the Oven"
+      card-title="News from the Oven >"
       image-link="url(https://cdn.pixabay.com/photo/2016/11/29/08/24/bakery-1868396_1280.jpg)"
       link-to="/news"
     />
     <HomeImageComponent
-      card-title="Shop Locations"
+      card-title="Shop Locations >"
       image-link="url(https://images.unsplash.com/photo-1587241321921-91a834d6d191?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)"
       link-to="/locations"
     />
     <HomeImageComponent
-      card-title="Explore Products"
+      card-title="Explore Products >"
       image-link="url(https://images.unsplash.com/photo-1509440159596-0249088772ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80)"
       link-to="/products"
     />
     <HomeImageComponent
-      card-title="History"
+      card-title="History >"
       image-link="url(https://i.pinimg.com/originals/39/b5/e4/39b5e46f65e6783e4cb52f0edd2a3bfb.jpg)"
       link-to="/history"
     />
@@ -30,7 +30,7 @@
   display: grid;
   grid-template-columns: 300px;
   padding: 100px 0px;
-  justify-content: space-around;
+  justify-content: center;
   width: 100%;
   margin: -0.7rem auto;
   background-color: antiquewhite;
@@ -41,8 +41,8 @@
   }
 
   @media screen and (min-width: 1250px) {
-    grid-template-columns: repeat(4, 300px);
-    gap: 1rem;
+    grid-template-columns: repeat(2, 500px);
+    gap: 5rem;
   }
 }
 </style>
