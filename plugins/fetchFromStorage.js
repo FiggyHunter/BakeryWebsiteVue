3f804a505280c17ba42c31c144e3f4f127601b45
figgyhunter <leonardo.roi3@gmail.com>
2023-06-15 22:25:42 +0200

    products now prefetched when the page loads

--- plugins/fetchFromStorage.js
+++ plugins/fetchFromStorage.js
@@ -1,8 +1,10 @@
+import { useProductsStore } from '~/stores/products';
 import { useUserStore } from '../stores/user';
 
 export default defineNuxtPlugin(async () => {
   if (process.client) {
     const userStore = useUserStore();
+    const productStore = useProductsStore();
 
     if (localStorage.getItem('cartItems')) {
       userStore.REPLACE_CART(JSON.parse(localStorage.getItem('cartItems')));
@@ -11,5 +13,7 @@ export default defineNuxtPlugin(async () => {
     if (localStorage.getItem('popup_displayed')) {
       userStore.GET_POPUP.value = JSON.parse(localStorage.getItem('popup_displayed'));
     }
+    productStore.FETCH_PRODUCTS();
+    productStore.FETCH_WEBSHOP_PRODUCTS();
   }
 });
