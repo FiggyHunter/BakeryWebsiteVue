c561ce526b59a95817adf65abb0e59cb7c7daeb2
figgyhunter <leonardo.roi3@gmail.com>
2023-06-15 22:28:13 +0200

    started working on order content

---
+++ components/order/OrderContent.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="order-details">
+    <OrderCartItems />
+    <OrderForm />
+  </main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.order-details {
+  display: grid;
+  margin-bottom: clamp(7rem, 1rem + 10vw, 10rem);
+  @media screen and (min-width: 1000px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  gap: 4rem;
+}
+</style>
